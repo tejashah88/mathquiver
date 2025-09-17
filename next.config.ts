bd02689ac6b1af75a84abf5cf0f5d392b0d57692
Tejas Shah <tejashah88@gmail.com>
2025-09-17 07:55:11 -0700

    devlog 2: added offline support with 'next-pwa'

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,23 @@
-import type { NextConfig } from "next";
+import withPWAInit, { PWAConfig } from "next-pwa";
 
-const nextConfig: NextConfig = {
+// Enable offline support out-of-the-box
+const withPWA = withPWAInit({
+  dest: "public",         // output service worker here
+  register: true,         // auto register the service worker
+  skipWaiting: true,      // activate new SW immediately
+});
+
+const nextConfig: PWAConfig = withPWA({
   reactStrictMode: true,
 
   // Export Settings
   output: 'export',
   assetPrefix: './',
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
 
   // GitHub Pages Settings
   basePath: "/mathquiver",
-};
+});
 
 export default nextConfig;
