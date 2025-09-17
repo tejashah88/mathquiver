396c5afab5f7cff9f361c32d889ea1b552cc9211
Tejas Shah <tejashah88@gmail.com>
2025-09-17 10:49:38 -0700

    devlog 5: ditching PWA

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,17 @@
 import { NextConfig } from "next";
 
-import withPWAInit, { PWAConfig } from "next-pwa";
-
-const isExport = process.env.NEXT_STATIC_EXPORT === "true";
-
-// Enable offline support out-of-the-box
-const withPWA = withPWAInit({
-  dest: "public",         // output service worker here
-  register: true,         // auto register the service worker
-  skipWaiting: true,      // activate new SW immediately
-});
-
-const config: PWAConfig = withPWA({
+const config: NextConfig = {
   reactStrictMode: true,
 
   // Export Settings
   output: 'export',
-  assetPrefix: isExport ? './' : undefined,
+  // assetPrefix: './',
   trailingSlash: true,
   images: { unoptimized: true },
 
   // GitHub Pages Settings
-  basePath: isExport ? "/mathquiver" : undefined,
-});
+  basePath: '/mathquiver',
+};
+console.log(config);
 
 export default config;
