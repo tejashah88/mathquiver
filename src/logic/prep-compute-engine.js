b307fb77a12a15ab240fd6bbbee0f9541a75de01
Tejas Shah <tejashah88@gmail.com>
2025-10-18 11:35:36 -0700

    fixed: sometimes reloading the webpage too fast in dev mode triggers a mathlive re-declaration error

--- src/logic/prep-compute-engine.js
+++ src/logic/prep-compute-engine.js
@@ -14,7 +14,13 @@ const EXCLUDE_CONSTANTS = ['CatalanConstant', 'GoldenRatio', 'EulerGamma'];
 
 function setupExtendedAlgebraMode(ce) {
     // Disable the single character functions to allow normal algebra usage
-    EXCLUDE_FUNCTIONS.forEach(funcName => ce.declare(funcName, 'unknown'));
+    EXCLUDE_FUNCTIONS.forEach(funcName => {
+        // NOTE: Trying to re-declare a variable will throw an error
+        // Only seems to happen when website is not fully unloaded and trying to reload in dev mode
+        // Source: https://github.com/cortex-js/compute-engine/blob/690a56eaef725d436b2ffc4eeee787de03b219fd/src/compute-engine/index.ts#L1388
+        if (!ce.context.lexicalScope.bindings.has(funcName))
+            ce.declare(funcName, 'unknown');
+    });
 
     // Disable defined constants to ensure Excel conversion works in a more algebraic manner
     ce.latexDictionary = ce.latexDictionary.filter(entry => !EXCLUDE_CONSTANTS.includes(entry.name));
